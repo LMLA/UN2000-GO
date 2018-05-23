0f577d6ff1a5b5d3add58c47cdc11c5b3083fa4e
pablo castaño <evengodsmustdie@gmail.com>
2018-05-23 15:19:55 -0500

    agregar documentacion receive

--- receive.go
+++ receive.go
@@ -13,6 +13,7 @@ import (
 	"github.com/secsy/goftp"
 )
 
+// variables globales
 var Lr bool
 var responser []byte
 var errr error
@@ -20,39 +21,43 @@ var ENQr = byte(0x05)
 var EOTr = byte(0x04)
 var ACKr = byte(0x06)
 
+// funcion para subir txt al servidor ftp
 func UploadFTP(client *goftp.Client, filename string, location string) error {
-	bigFile, err := os.Open(filename) //"./test/valid/" + filename
+	bigFile, err := os.Open(filename) // "result/" + filename
 	if err != nil {
 		return err
 	}
 
-	err = client.Store(location, bigFile) //"iib/test/pmla/"+filename
+	err = client.Store(location, bigFile) // "iib/071/" + filename
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
 
+// funcion que verifica si el mensaje recibido es un fin de mensaje
 func verifyQueryReceive(message string) (L bool, response []byte, err error) {
 	L = false
 	response = []byte{0x06}
 	err = nil
-	verify := message[2:3]
-	if verify == "L" {
+	verify := message[2:3] // toma el nombre del campo
+	if verify == "L" { // compara si el campo es un fin de mensaje
 		L = true
 	}
-		response = []byte{0x06}
-		err = errors.New(verify)
+		response = []byte{0x06} // responde ACK
+		err = errors.New(verify) // almacena el nombre del campo
 
 	return L, response, err
 }
 
+// Funcion principal
 
+// Recibe una serie de resultados en formato ASTM por cada ACK enviado luego de iniciado la comunicacion
+// almacena los resultados en cadenas de texto para ser enviadas por FTP para ser almacenadas por 4D
 func main() {
 	fmt.Println("Cargando Servidor...")
-
-	err := godotenv.Load(".env")
+	// carga archivo .env con variables dinamicas
+	err := godotenv.Load(".env") // error de carga
 	if err != nil {
 		log.Fatal("Error cargando archivo .env")
 	}
@@ -62,19 +67,11 @@ func main() {
 
 
 
-	// String conexion MySQL
+	// String conexion TCP
 
 	ln, _ := net.Listen("tcp", ":"+ tcpPort)
 
-Retry:
-
-// Acepta condiciento en puerto indicado
-	conn, err := ln.Accept()
-	fmt.Println(conn.RemoteAddr().String())
-	if err != nil {
-		fmt.Println("error tcp", err)
-	}
-
+	// configuracion conexion FTP
 	config := goftp.Config{
 		User:               "conlab97",
 		Password:           "lab3000",
@@ -83,10 +80,20 @@ Retry:
 		Logger:             os.Stderr,
 	}
 
+Retry:
+
+// Acepta conexion en puerto indicado
+	conn, err := ln.Accept()
+	fmt.Println(conn.RemoteAddr().String()) // imprime ip de cliente
+	if err != nil { // error tcp
+		fmt.Println("error tcp", err)
+	}
+
+	// crea instancia de conexion ftp
 	client, ftpconnerr := goftp.DialConfig(config, ftpAddress)
-	if ftpconnerr != nil {
+	if ftpconnerr != nil { // error FTP
 		fmt.Println(ftpconnerr)
-		c := time.Tick(10 * time.Second) // Reconexion TCP
+		c := time.Tick(10 * time.Second) // Reconexion FTP
 		for now := range c {
 			fmt.Println(now)
 			goto Retry
@@ -104,12 +111,12 @@ Retry:
 		fmt.Println("Inicio mensaje")
 		t := time.Now()
 		timestamp := t.Format("20060102150405")
-		filename := timestamp+".txt"
-		// ENQ
+		filename := timestamp+".txt" // nombre archivo <AAAAMMDDhhmmss>.txt
+		// ENQ lectura
 		message, err := bufio.NewReader(conn).ReadString(ENQr)
-		if err != nil {
+		if err != nil { // error de ENQ
 			fmt.Println("timeout") // Manejo de errores
-			if io.EOF == err {
+			if io.EOF == err { // conexion perdida
 				fmt.Println("connection dropped message", err)
 				goto Retry
 			}
@@ -126,7 +133,7 @@ Retry:
 					fmt.Println("desconectado") // Manejo de errores
 					break // Sale del loop si se desconecta el cliente
 				} else {
-					// If the file doesn't exist, create it, or append to the file
+					// si el archivo no existe lo crea, si ya existe agrega texto
 					f, err := os.OpenFile("results/"+filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 					if err != nil {
 						log.Fatal(err)
@@ -137,7 +144,7 @@ Retry:
 					if err := f.Close(); err != nil {
 						log.Fatal(err)
 					}
-					Lr, responser, err = verifyQueryReceive(message)
+					Lr, responser, err = verifyQueryReceive(message) // funcion para verificar fin del mensaje
 				}
 				if err != nil {
 					time.Sleep(100 * time.Millisecond)
@@ -148,12 +155,12 @@ Retry:
 					conn.Write(responser)
 				}
 
-				if Lr == true {
-					// EOT
+				if Lr == true { // fin mensaje
+					// EOT lectura
 					message, err = bufio.NewReader(conn).ReadString(EOTr)
 					if err != nil {
 						fmt.Println("desconectado") // Manejo de errores
-						break // Sal*e del loop si se desconecta el cliente
+						break // Sale del loop si se desconecta el cliente
 					} else {
 						fmt.Println("Fin mensaje")
 						time.Sleep(100 * time.Millisecond)
@@ -161,6 +168,7 @@ Retry:
 					}
 				}
 			}
+			// funcion para enviar por ftp
 			ftpUpErr := UploadFTP(client, "results/" + filename, "iib/071/"+filename)
 			if ftpUpErr != nil {
 				fmt.Println(ftpUpErr)
